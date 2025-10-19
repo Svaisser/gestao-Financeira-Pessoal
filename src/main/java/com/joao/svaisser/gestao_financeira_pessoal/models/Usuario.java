ead06e3d7e446cd784cbd974155117c259fc7a47
SvaisserBetha <joao.svaisser@betha.com.br>
2025-10-19 19:01:35 -0300

    Conectado com o banco Supabase

---
+++ src/main/java/com/joao/svaisser/gestao_financeira_pessoal/models/Usuario.java
@@ -0,0 +1,54 @@
+package com.joao.svaisser.gestao_financeira_pessoal.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String senha;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
